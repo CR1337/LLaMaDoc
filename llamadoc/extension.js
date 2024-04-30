cb049cb55d2271a65d2733189cb8e369a9b0f384
lkursawe <lara.kursawe@uni-potsdam.de>
2024-04-30 15:57:30 +0200

    Extension now runs python script

--- llamadoc/extension.js
+++ llamadoc/extension.js
@@ -1,36 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+const path = require('path')
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+
 function activate(context) {
 
-	let button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	button.text = "Button Label";
-	button.command = "llamadoc.helloWorld";
+	let button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	button.text = "$(megaphone) Check for Out of Date Docstrings";
+	button.command = "llamadoc.scanFile";
 	button.show();
 
+	let disposable = vscode.commands.registerCommand('llamadoc.scanFile', function () {
+		
+		const { exec } = require('child_process');
+		const argument = vscode.window.activeTextEditor.document.fileName;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "llamadoc" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('llamadoc.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LLaMaDoc!');
+		exec(`python -u ${__dirname}\\find_docstrings.py "${argument}"`, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`Error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.error(`stderr: ${stderr}`);
+			}
+			console.log(`stdout: ${stdout}`);
+			let jsonObject = JSON.parse(stdout)
+		});
 	});
 
 	context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
