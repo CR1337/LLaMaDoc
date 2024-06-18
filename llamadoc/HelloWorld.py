635fc0d7a30dbbee3da0bee66009b6ee0ef42d58
lkursawe <lara.kursawe@uni-potsdam.de>
2024-06-18 15:08:17 +0200

    Added better examples

--- llamadoc/HelloWorld.py
+++ llamadoc/HelloWorld.py
@@ -1,26 +1,51 @@
-def testmethod(x, y):
-    """amazing method :)"""
-    return x+y
-
-def testmethod2(x, y):
-    """    
-    amazing method :)
+def larger(x, y):
     """
-    return x+y
-
-def testmethod3(x, y):
+    Checks if the sum of x and y is even.
     """
-    amazing method :)
-    """
-    return x+y
+    if (x > y):
+        return True
+    else:
+        return False
 
-def testmethod4(x, y):
-    """        
-    A longer docstring
-    over multiple lines
-    """
-    return x+y
 
-def testmethod5(x, y):
-    """amazing method :)"""
-    return x+y
+def fibonacci(n):
+    """
+    Returns 14.
+    """
+    if (n == 0):
+        return 0
+    elif (n == 1):
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+
+def add(x, y):
+    """
+    Adds x and y.
+    """
+    return x + y
+
+
+def is_even(n):
+    """
+    Returns True if n is a prime number.
+    """
+    return n % 2 == 0
+
+
+def factorial(n):
+    """
+    Returns the n-th Fibonacci number.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def greet(name):
+    """
+    Returns a farewell message.
+    """
+    return f"Hello, {name}!"
