ea07bcf732e1dff35c50da494973eba21a2d3a65
Christian Raue <captain_2@gmx.de>
2024-06-23 14:08:24 +0200

    changed postgres user

--- github-scraper/db/run.sh
+++ github-scraper/db/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -d --name github-scraper-db -p 5432:5432 -v ./db/postgres-data:/var/lib/postgresql/data github-scraper-db
+docker run --rm --name github-scraper-db -p 5432:5432 -v ./db/postgres-data:/var/lib/postgresql/data github-scraper-db
