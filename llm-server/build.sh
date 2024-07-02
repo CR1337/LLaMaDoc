23883916acadb9c2c54b50bb35dd84b169676a6d
Christian Raue <captain_2@gmx.de>
2024-07-02 14:18:00 +0200

    reverted user change

--- llm-server/build.sh
+++ llm-server/build.sh
@@ -2,7 +2,7 @@
 
 echo "Building container..."
 
-docker build --build-arg UID=$(id -u) -t llm_server_llamadoc .
+docker build -t llm_server_llamadoc .
 
 if [ $? -ne 0 ]; then
         echo "Failed to build container."
