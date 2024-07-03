0e7cfe28d939fdc80bfd5ef21dfa776de9ad0090
Christian Raue <captain_2@gmx.de>
2024-07-03 13:02:12 +0200

    bugfix

--- llm-server/generate_dummy_token_frequencies.py
+++ llm-server/generate_dummy_token_frequencies.py
@@ -4,8 +4,8 @@ from math import log
 N = 256000
 e = 1e-16
 
-frequencies = (1.0 for _ in range(N))
-log_frequencies = (log(frequency) for frequency in frequencies)
+frequencies = (10.0 for _ in range(N))
+log_frequencies = (log(frequency + e) for frequency in frequencies)
 relative_log_frequencies = (
     log_frequency / max(log_frequencies) 
     for log_frequency in log_frequencies
@@ -15,5 +15,5 @@ inverse_relative_log_frequencies = (
     for relative_log_frequency in relative_log_frequencies
 )
 
-with open("inverse_relative_log_token_frequencies.pkl", "wb") as file:
+with open("inverse_relative_log_token_frequencies_DUMMY.pkl", "wb") as file:
     pickle.dump(list(inverse_relative_log_frequencies), file)
