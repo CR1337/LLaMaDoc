fc329e5cea9ab782a5e15eff2d77dfa8a1149c49
Christian Raue <captain_2@gmx.de>
2024-07-05 12:29:03 +0200

    rebuild script

---
+++ llm-server/rebuild.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def main():
+    process = subprocess.Popen(['docker', 'ps', '-a'], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    output = stdout.decode('utf-8')
+    for line in output.split('\n'):
+        if 'llm_server_llamadoc' in line:
+            container_id = line.split()[0]
+            break
+
+    process = subprocess.Popen(['docker', 'stop', container_id], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    print(stdout.decode('utf-8'))
+
+    process = subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    print(stdout.decode('utf-8'))
+
+    process = subprocess.Popen(['sh', 'build.sh'], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    print(stdout.decode('utf-8'))
+    
