7994b117d3426ef566bdaa870ccf4345768f6924
Christian Raue <captain_2@gmx.de>
2024-07-06 13:24:54 +0200

    removed linebreak

--- llm-server/run-blocking.sh
+++ llm-server/run-blocking.sh
@@ -9,8 +9,7 @@ echo "Creating container..."
 
 docker run --rm --init --name ai4se-2-llm-server --gpus device=$GPU \
         -p 0.0.0.0:$PORT:8000 -v $CACHE_DIR:/root/.cache/huggingface \
-        -v $(pwd)/cache:/server/cache -v $(pwd)/checkpoints:/server/checkpoints \ 
-        llm_server_llamadoc
+        -v $(pwd)/cache:/server/cache -v $(pwd)/checkpoints:/server/checkpoints llm_server_llamadoc
 
 if [ $? -ne 0 ]; then
         echo "Failed to create container."
